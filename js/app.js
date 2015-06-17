ef5576c2abf9ff10faa616b0ee08ef91464d3220
Wayne Wang <lastr2d2@gmail.com>
2015-06-17 16:51:50 +0800

    v 1.0

--- js/app.js
+++ js/app.js
@@ -5,46 +5,122 @@
 var app = {
     clock: undefined,
     selector: '#countdown',
-    totalSeconds: 3,
-    lastSeconds: 1
+    totalSeconds: 50,
+    lastSeconds: 8,
+    remainingSeconds: undefined
 };
+
 app.init = function () {
     app.clock = $(app.selector).FlipClock(app.totalSeconds, {
-            clockFace: 'HourlyCounter',
-            countdown: true,
-            autoStart: false,
-            callbacks: {
-                interval: function () {
-                    var time = this.factory.getTime().time;
-                    if (time) {
-                        app.playAudioInLastTenSeconds(time);
+        clockFace: 'HourlyCounter',
+        countdown: true,
+        autoStart: false,
+        callbacks: {
+            interval: function () {
+                var time = this.factory.getTime().time;
+                if (time) {
+                    app.playAudioInLastTenSeconds(time);
+                    app.remainingSeconds = time;
+                }
+            }, stop: function () {
+                app.applyPausedStyle();
+            }
+        }
+    });
+    app.bindEvents();
+    app.applyPausedStyle();
+};
+
+app.bindEvents = function () {
+    $('#startButton').click(function () {
+        if (app.clock.running) {
+            app.clock.stop();
+            app.applyPausedStyle();
+        }
+        else {
+            if (app.remainingSeconds <= 1) {
+                app.clock.setTime(app.totalSeconds);
+            }
+            app.clock.start();
+            app.applyRunningStyle();
+        }
+    });
+
+    $('#resetButton').click(function () {
+        app.clock.stop();
+        app.clock.setTime(app.totalSeconds);
+        app.applyPausedStyle();
+    });
+
+    $('#setTotalSeconds').click(function () {
+
+        bootbox.prompt({
+            title: "Set Total Seconds of clock",
+            value: app.totalSeconds,
+            callback: function (result) {
+                if (result === null) {
+                    //Example.show("Prompt dismissed");
+                } else {
+                    var parsedResult = parseInt(result);
+                    if (parsedResult) {
+                        app.totalSeconds = parsedResult;
+                        app.clock.stop();
+                        app.clock.setTime(app.totalSeconds);
                     }
                 }
             }
-        }
-    );
-    app.bindEvent();
+        });
+    });
+
+    $('#setFinalSeconds').click(function () {
+        bootbox.prompt({
+            title: "Set Final Seconds of clock",
+            value: app.totalSeconds,
+            callback: function (result) {
+                if (result === null) {
+                    //Example.show("Prompt dismissed");
+                } else {
+                    var parsedResult = parseInt(result);
+                    if (parsedResult) {
+                        app.lastSeconds = parsedResult;
+                        app.clock.stop();
+                    }
+                }
+            }
+        });
+    });
+
 };
 
-app.bindEvent = function () {
-    $('#start').click(function () {
-        app.clock.start();
-    });
-    $('#pause').click(function () {
-        app.clock.stop();
-    });
-    $('#reset').click(function () {
-        app.clock.reset();
-    });
+app.applyRunningStyle = function () {
+    if (!app.clock.running)
+        return;
+
+    $('#startButton').text('PAUSE')
+    $('#resetButton').removeAttr('disabled');
+    $('#advancedButtonsContainer').hide('fast');
+    $('#resetButton').show('fast');
+};
+
+app.applyPausedStyle = function () {
+    if (app.clock.running)
+        return;
+
+    $('#startButton').text('START');
+    $('#resetButton').attr('disabled', 'disabled');
+    $('#resetButton').hide('fast');
+    $('#advancedButtonsContainer').show('fast');
 };
 
 app.playAudioInLastTenSeconds = function (seconds) {
     var $audio = $('#lastSeconds')[0];
-    console.log(seconds);
-
     if (seconds > app.lastSeconds)
         return;
-    console.log('play');
+
+    app.stopCurrentAudioAndPlayAgain($audio);
+};
+
+app.stopCurrentAudioAndPlayAgain = function ($audio) {
     $audio.pause();
     $audio.currentTime = 0;
     $audio.play();
